ff0c04fa10598254bbe7c2d0544ad58a68aadf9c
Noëllie Chatain Six <noellie.chatain-six@oclock.school>
2024-03-17 12:53:51 +0100

    perform initial commit to remote after local configuration

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+// import frameworks and libraries
+import React from 'react';
+
+// import UI Components
+
+// import custom Components
+
+// import stylesheets
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <p>
+        Hello World!
+      </p>
+    </div>
+  );
+}
+
+export default App;
