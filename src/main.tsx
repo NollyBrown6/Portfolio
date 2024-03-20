c3dd55257b0d3ec6cf54280d5b971c8d84c26f90
Noëllie Chatain Six <noellie.chatain-six@oclock.school>
2024-03-20 17:20:35 +0100

    import stylesheets: reset, index and rsuite

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
+
+// import stylesheets
+import '../public/style/reset.scss';
+import '../public/style/index.scss';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'rsuite/dist/rsuite.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
