6e93b3227a89494539abad53e1095cbcbac855f8
Noëllie Chatain Six <noellie.chatain-six@oclock.school>
2024-03-20 17:28:28 +0100

    create navigation buttons pointing to different routes

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -18,6 +18,23 @@ function Header() {
           /NC6 /
         </h1>
       </div>
+      <nav className="navbar-container">
+        <a href="/about">
+          <button className="button" type="button">À propos</button>
+        </a>
+        <a href="experience">
+          <button className="button" type="button">Formation</button>
+        </a>
+        <a href="/projects">
+          <button className="button" type="button">Projets</button>
+        </a>
+        <a href="/sandbox">
+          <button className="button" type="button">Sandbox</button>
+        </a>
+        <a href="/contact">
+          <button className="button" type="button">Contact</button>
+        </a>
+      </nav>
     </div>
   );
 }
