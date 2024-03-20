5b38327e79148991d71df45965c3e8481b20b34f
Noëllie Chatain Six <noellie.chatain-six@oclock.school>
2024-03-20 17:37:49 +0100

    create ComputerAnimation component from external project and related stylesheet

---
+++ src/Components/HomeContent/ComputerAnimation/ComputerAnimation.tsx
@@ -0,0 +1,258 @@
+// import frameworks and libraries
+import React from 'react';
+
+// import UI Components
+
+// import custom Components
+
+// import stylesheets
+import './ComputerAnimation.scss';
+
+function ComputerAnimation() {
+  return (
+    <div className="object-container">
+      <div className="screen">
+        <div className="section site-section">
+          <div className="site-window-container">
+            <div className="topbar-container">
+              <div className="btns-container">
+                <div className="btn btn-close" />
+                <div className="btn btn-hide" />
+                <div className="btn btn-expand" />
+              </div>
+              <div className="searchbar-container">
+                <div className="searchbar" />
+              </div>
+            </div>
+            <div className="body-container">
+              <div className="content-container">
+                <div className="text-container">
+                  <div className="text-item text-title" />
+                  <div className="separator-xs" />
+                  <div className="text-item text-lg" />
+                  <div className="text-item text-md" />
+                  <div className="text-item text-sm" />
+                  <div className="text-item text-md" />
+                </div>
+                <div className="cards-container">
+                  <div className="card">
+                    <div className="card-content" />
+                    <div className="card-footer" />
+                  </div>
+                  <div className="card">
+                    <div className="card-content" />
+                    <div className="card-footer" />
+                  </div>
+                  <div className="card">
+                    <div className="card-content" />
+                    <div className="card-footer" />
+                  </div>
+                </div>
+              </div>
+              <div className="separator-m" />
+              <div className="content-container">
+                <div className="cards-container">
+                  <div className="card">
+                    <div className="card-content" />
+                    <div className="card-footer" />
+                  </div>
+                  <div className="card">
+                    <div className="card-content" />
+                    <div className="card-footer" />
+                  </div>
+                </div>
+                <div className="text-container">
+                  <div className="text-item text-md" />
+                  <div className="text-item text-lg" />
+                  <div className="text-item text-sm" />
+                  <div className="text-item text-sm" />
+                  <div className="text-item text-md" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="section code-interface-section">
+          <div className="code-container">
+            <div className="code-content">
+              <div className="line">
+                <div className="item function lg" />
+                <div className="item variable lg" />
+                <div className="item content md" />
+              </div>
+              <div className="line">
+                <div className="item function md" />
+                <div className="item variable sm" />
+              </div>
+              <div className="line">
+                <div className="item function md" />
+                <div className="item variable lg" />
+                <div className="item content xlg" />
+              </div>
+              <div className="line">
+                <div className="item function lg" />
+                <div className="item other xlg" />
+              </div>
+              <div className="line tab1">
+                <div className="item other md" />
+                <div className="item variable sm" />
+                <div className="item content xlg" />
+              </div>
+              <div className="line tab2">
+                <div className="item other md" />
+                <div className="item variable sm" />
+                <div className="item content md" />
+              </div>
+              <div className="line tab2">
+                <div className="item other md" />
+                <div className="item variable sm" />
+                <div className="item content xlg" />
+              </div>
+              <div className="line tab3">
+                <div className="item other md" />
+                <div className="item content xxlg" />
+                <div className="item other s" />
+              </div>
+              <div className="line">
+                <div className="item function md" />
+                <div className="item variable lg" />
+                <div className="item content xlg" />
+                <div className="item other xlg" />
+                <div className="item other s" />
+              </div>
+              <div className="line tab1">
+                <div className="item function md" />
+                <div className="item content xxlg" />
+                <div className="item other xxlg" />
+              </div>
+              <div className="line tab1">
+                <div className="item function md" />
+                <div className="item variable xlg" />
+                <div className="item content lg" />
+                <div className="item other xxlg" />
+                <div className="item other md" />
+              </div>
+              <div className="line tab2">
+                <div className="item other lg" />
+                <div className="item variable lg" />
+              </div>
+              <div className="line tab3">
+                <div className="item other sm" />
+                <div className="item variable lg" />
+                <div className="item content md" />
+              </div>
+              <div className="line tab3">
+                <div className="item other md" />
+                <div className="item variable sm" />
+                <div className="item content xlg" />
+              </div>
+              <div className="line tab2">
+                <div className="item other md" />
+                <div className="item variable sm" />
+                <div className="item content xlg" />
+                <div className="item other sm" />
+                <div className="item other lg" />
+              </div>
+              <div className="line">
+                <div className="item function md" />
+                <div className="item content xlg" />
+              </div>
+              <div className="line tab2">
+                <div className="item other sm" />
+                <div className="item variable sm" />
+                <div className="item content xlg" />
+              </div>
+              <div className="line tab2">
+                <div className="item other md" />
+                <div className="item other xsm" />
+                <div className="item variable sm" />
+                <div className="item content lg" />
+              </div>
+              <div className="line">
+                <div className="item function md" />
+                <div className="item variable xsm" />
+                <div className="item content xxlg" />
+              </div>
+              <div className="line">
+                <div className="item function xxlg" />
+                <div className="item variable sm" />
+                <div className="item other lg" />
+                <div className="item other md" />
+              </div>
+              <div className="line tab1">
+                <div className="item other md" />
+                <div className="item content xlg" />
+              </div>
+              <div className="line">
+                <div className="item function md" />
+                <div className="item other lg" />
+                <div className="item content xxlg" />
+              </div>
+              <div className="line tab1">
+                <div className="item other xsm" />
+                <div className="item variable sm" />
+                <div className="item content md" />
+              </div>
+              <div className="line tab2">
+                <div className="item function xlg" />
+                <div className="item variable sm" />
+                <div className="item content xlg" />
+              </div>
+              <div className="line tab3">
+                <div className="item other md" />
+                <div className="item variable sm" />
+                <div className="item content xlg" />
+              </div>
+              <div className="line tab2">
+                <div className="item function xlg" />
+                <div className="item other md" />
+                <div className="item content xlg" />
+              </div>
+              <div className="line tab2">
+                <div className="item function lg" />
+                <div className="item content xxlg" />
+                <div className="item other md" />
+                <div className="item other sm" />
+              </div>
+              <div className="line tab2">
+                <div className="item function md" />
+                <div className="item variable sm" />
+                <div className="item content xlg" />
+              </div>
+              <div className="line">
+                <div className="item function md" />
+                <div className="item content sm" />
+                <div className="item other xlg" />
+                <div className="item other xlg" />
+              </div>
+              <div className="line">
+                <div className="item function md" />
+                <div className="item variable xlg" />
+                <div className="item content xlg" />
+              </div>
+              <div className="line tab2">
+                <div className="item other md" />
+                <div className="item content xlg" />
+              </div>
+              <div className="line tab2">
+                <div className="item other md" />
+                <div className="item other xxlg" />
+                <div className="item content xlg" />
+              </div>
+              <div className="line tab1">
+                <div className="item function lg" />
+                <div className="item variable xlg" />
+                <div className="item content xxlg" />
+                <div className="item other xsm" />
+                <div className="item other lg" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+export default ComputerAnimation;
