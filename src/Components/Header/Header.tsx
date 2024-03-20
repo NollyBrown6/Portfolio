6902df815da2d8cc2d70ccde9d1f661c8be7931c
Noëllie Chatain Six <noellie.chatain-six@oclock.school>
2024-03-20 17:13:16 +0100

    create Header component (static)

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,25 @@
+// import frameworks and libraries
+import React from 'react';
+
+// import UI Components
+// eslint-disable-next-line import/no-extraneous-dependencies
+
+// import custom Components
+
+// import stylesheets
+import './Header.scss';
+
+function Header() {
+  return (
+    <div className="header-container">
+      <div className="separator v-m" />
+      <div className="title-container">
+        <h1 className="title-text">
+          /NC6 /
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
