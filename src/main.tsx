29bef24a642f2902a2a6f561f3545ba23df89f90
Noëllie Chatain Six <noellie.chatain-six@oclock.school>
2024-03-20 18:37:04 +0100

    create router from appRouter and use RouteProvider to pass router to ReactDOM

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import { RouterProvider } from 'react-router-dom';
+import appRouter from './routes/router.tsx';
 
 // import stylesheets
 import '../public/style/reset.scss';
@@ -8,8 +9,10 @@ import '../public/style/index.scss';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import 'rsuite/dist/rsuite.min.css';
 
+const router = appRouter;
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
